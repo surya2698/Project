fe49108c3b03e959e0b2c3a130ddfab080db371d
surya2698 <surya.chandran02@outlook.com>
2022-01-29 13:38:24 +0530

    prakash updated

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -3,8 +3,10 @@ package org.test;
 public class Sample {
 
 	public static void main(String[] args) {
-System.out.println("welcome to github");
+		System.out.println("welcome to github");
+		System.out.println("welcome to prakash");
+		System.out.println("welcome to hi");
+
 	}
-	
 
 }
